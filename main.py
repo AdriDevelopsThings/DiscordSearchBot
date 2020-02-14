9368bf66381f49de0f6c4b6c0d586ee32e431d2e
Adrian Bloober <adrianbloober@gmail.com>
2020-02-14 19:24:29 +0100

    add google reaction

--- main.py
+++ main.py
@@ -39,5 +39,23 @@ async def on_message(message):
         link = "https://lmgtfy.com/?q=" + content
         await message.channel.send(link)
 
+invalid = []
+
+@bot.event
+async def on_reaction_add(reaction, user):#
+    if reaction.count != 1 or reaction.message.id in invalid:
+        return
+    if str(reaction) == "<:google:331075369501196290>" or str(reaction) == "<:google:677939519160451093>":
+        content = reaction.message.content
+        s = api.search(content)
+        invalid.append(reaction.message.id)
+        if s is None:
+            await reaction.message.channel.send("0 Ergebnisse gefunden")
+        else:
+            embed = discord.Embed(title="Google suche", description=content)
+            for value in s:
+                embed.add_field(name=value["title"], value=value["link"], inline=False)
+            await reaction.message.channel.send(embed=embed)
+
 
 bot.run(settings.DISCORD_BOT_TOKEN.get_string())
