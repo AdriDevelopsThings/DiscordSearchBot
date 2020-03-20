1e710b05091377ccd8fadca716e142d3fa2b1d73
Adrian Bloober <adrianbloober@gmail.com>
2020-03-20 22:34:30 +0100

    Fix https://github.com/AdriBloober/DiscordSearchBot/issues/36 Fix https://github.com/AdriBloober/DiscordSearchBot/issues/37

--- resources/core/errors/errors.py
+++ resources/core/errors/errors.py
@@ -1,5 +1,12 @@
 from discord.ext.commands import MissingRequiredArgument, BadArgument, CommandNotFound, CommandInvokeError
 
+from resources.core.logger import get_logger
+
+
+async def send_error_to_log(error, guild):
+    logger = get_logger()
+    await logger.log(f"Auf dem Server {guild.name} ist der Fehler {error.__class__} aufgetreten.")
+
 
 async def parse_error(error, channel):
     if isinstance(error, MissingRequiredArgument) or isinstance(error, BadArgument):
@@ -11,11 +18,12 @@ async def parse_error(error, channel):
         return False
     elif isinstance(error, CommandInvokeError):
         print(error.__cause__)
+        await send_error_to_log(error, channel.guild)
         await channel.send(
             f"Der Fehler {error.__cause__} ist aufgetreten. Bitte informiere die Projektleitung vom DiscordSearchBot!"
         )
         return True
-
+    await send_error_to_log(error, channel.guild)
     await channel.send(
         f"Der Fehler {error.__class__} ist aufgetreten. Bitte informiere die Projektleitung vom DiscordSearchBot!"
     )
