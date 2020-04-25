d4e435cf7f26e992604f52f372bcc0af15233264
Adrian Bloober <adrianbloober@gmail.com>
2020-04-25 16:39:10 +0200

    Fix lmgtfy

--- resources/core/commands/lmgtfy.py
+++ resources/core/commands/lmgtfy.py
@@ -1,5 +1,5 @@
 from urllib.parse import quote
 
 
-def lmgtfy_api(ctx, q):
-    ctx.send("https://lmgtfy.com/?q=" + quote(q).replace("%20", "+"))
+async def lmgtfy_api(ctx, q):
+    await ctx.send("https://lmgtfy.com/?q=" + quote(q).replace("%20", "+"))
